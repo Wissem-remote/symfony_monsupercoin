36a5e8c5b8cfc0c5733b14230614266d475b5bbe
rezki wissem <wissemrezki@gmail.com>
2022-09-20 09:47:13 +0200

    Version Email

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,8 @@ const onSubmit = document.querySelector('.email');
 const form = document.getElementById('formSubmitButton');
 
 onSubmit.addEventListener('click', (e) => {
+    e.preventDefault();
+    //console.log('hello');
     const value = Object.fromEntries(new FormData(e.path[1]));
     emailjs.send("service_gzwqu0t", "template_it42osy", value, "user_Q6xTN3DJd6AoRLdHBeMdt")
         .then((res) => {
@@ -9,5 +11,4 @@ onSubmit.addEventListener('click', (e) => {
         }, (err) => {
             console.log('error', err)
         });
-    
 })
